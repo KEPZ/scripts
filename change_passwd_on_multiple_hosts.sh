c6751a92b57940374397d0f902dbad5c34c0d2ed
Ivan Luzinov <i.luzinov@gmail.com>
2020-02-20 17:14:08 +0300

    Update change_passwd_on_multiple_hosts.sh

--- change_passwd_on_multiple_hosts.sh
+++ change_passwd_on_multiple_hosts.sh
@@ -1,1 +1,11 @@
-
+#!/usr/bin/env bash
+for server in $(cat list_hosts.txt)
+do
+echo $server
+ssh root@$server 'passwd <<EOF
+New_pass
+New_pass
+EOF';
+echo "Execution code - $?"
+echo '****************************************'
+done
